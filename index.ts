3c1c9ec58488963fedb7ebb054c875998798a5c3
ramiel <ramielwan48@gmail.com>
2023-05-02 16:31:54 +0300

    feat: basic done

--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-console.log(123 as unknown as string);
+import { create } from "zustand";
+
+type State = {
+  count: number;
+  increment: () => void;
+  decrement: () => void;
+};
+
+// type LoggerImpl = <T>(
+//   storeInitializer: StateCreator<T, [], []>
+// ) => StateCreator<T, [], []>;
+//
+// const loggerImpl: LoggerImpl = (config) => (set, get, api) => {
+//   return config(set, get, api);
+// };
+
+const store = create<State>()((set) => ({
+  count: 0,
+  increment: () => set((state: State) => ({ count: state.count + 1 })),
+  decrement: () => set((state: State) => ({ count: state.count - 1 })),
+}));
+store.getState().increment();
+console.log(store.getState());
