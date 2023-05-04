bc69f9966c230eec55cfb2ac0ab044c8e75032f5
ramiel <ramielwan48@gmail.com>
2023-05-04 15:59:23 +0300

    feat: rough impl

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { create } from "zustand";
+import { StateCreator, create } from "zustand";
 
 type State = {
   count: number;
@@ -6,18 +6,35 @@ type State = {
   decrement: () => void;
 };
 
-// type LoggerImpl = <T>(
-//   storeInitializer: StateCreator<T, [], []>
-// ) => StateCreator<T, [], []>;
-//
-// const loggerImpl: LoggerImpl = (config) => (set, get, api) => {
-//   return config(set, get, api);
-// };
+type LoggerImpl = <T>(
+  storeInitializer: StateCreator<T, [], []>
+) => StateCreator<T, [], []>;
 
-const store = create<State>()((set) => ({
-  count: 0,
-  increment: () => set((state: State) => ({ count: state.count + 1 })),
-  decrement: () => set((state: State) => ({ count: state.count - 1 })),
-}));
+const loggerImpl: LoggerImpl = (config) => (set, get, api) => {
+  const result = config(set, get, api);
+  if (typeof result === "object" && result !== null && result !== undefined) {
+    return Object.entries(result).reduce((acc, [key, value]) => {
+      if (typeof value === "function") {
+        acc[key] = () => {
+          value();
+          console.log(`${key} called`);
+        };
+      } else {
+        acc[key] = value;
+      }
+      return acc;
+    }, {}) as any;
+  } else {
+    return result;
+  }
+};
+
+const store = create<State>()(
+  loggerImpl((set) => ({
+    count: 0,
+    increment: () => set((state: State) => ({ count: state.count + 1 })),
+    decrement: () => set((state: State) => ({ count: state.count - 1 })),
+  }))
+);
 store.getState().increment();
 console.log(store.getState());
